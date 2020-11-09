365b565f6ebcc2d3e2c6dc9f7d638afe86de6f56
Łukasz Andrusiewicz <andrusiewiczl19263@student.pswbp.pl>
2020-11-10 00:37:04 +0100

    lab3 commit

---
+++ Calculator/IServicee1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Calculator
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IServicee1
+    {
+
+        [OperationContract]
+        Complex Add(double a_real, double a_imaginary, double b_real, double b_imaginary);
+
+        [OperationContract]
+        Complex Multiply(double a_real, double a_imaginary, double b_real, double b_imaginary);
+
+        [OperationContract]
+        Complex Substract(double a_real, double a_imaginary, double b_real, double b_imaginary);
+
+        [OperationContract]
+        [FaultContract(typeof(DivideByZeroException))]
+        Complex Divide(double a_real, double a_imaginary, double b_real, double b_imaginary);
+
+
+
+        // TODO: Add your service operations here
+    }
+
+    [DataContract]
+    public class DivideByZeroException
+    {
+        [DataMember]
+        public string Message { get; set; }
+        [DataMember]
+        public Complex Divided_complex_number { get; set; }
+ 
+
+    }
+
+
+    [DataContract]
+    public class Complex
+    {
+        double real;
+        double imaginary;
+
+        [DataMember]
+        public double Real
+        {
+            get { return real; }
+            set { real = value; }
+        }
+
+        [DataMember]
+        public double Imaginary
+        {
+            get { return imaginary; }
+            set { imaginary = value; }
+        }
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+   
+}
---
+++ Calculator/Service1.svc.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.Remoting.Messaging;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Calculator
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IServicee1
+    {
+
+        public Complex Add(double a_real, double a_imaginary, double b_real, double b_imaginary)
+        {
+            Complex c = new Complex();
+            c.Real = a_real + b_real;
+            c.Imaginary = a_imaginary + b_imaginary;
+            return c;
+        }
+
+        public Complex Divide(double a_real, double a_imaginary, double b_real, double b_imaginary)
+        {
+            if (b_real == 0 && b_imaginary == 0)
+            {
+
+                DivideByZeroException fault = new DivideByZeroException
+                {
+                    Message = "Cannot divide by zero!",
+                    Divided_complex_number = new Complex { Real = a_real, Imaginary = a_imaginary}
+                };
+                throw new FaultException<DivideByZeroException>(fault, new FaultReason(fault.Message));
+            }
+            Complex c = new Complex();
+            c.Real = ((a_real * b_real) + (a_imaginary * b_imaginary)) / (Math.Pow(b_real, 2) + Math.Pow(b_imaginary, 2));
+            c.Imaginary = ((a_imaginary * b_real) - (a_real * b_imaginary)) / (Math.Pow(b_real, 2) + Math.Pow(b_imaginary, 2));
+            return c;
+
+        }
+        
+
+        public Complex Multiply(double a_real, double a_imaginary, double b_real, double b_imaginary)
+        {
+            Complex c = new Complex();
+            c.Real = (a_real * b_real) - (a_imaginary * b_imaginary);
+            c.Imaginary = (a_real * b_imaginary) + (a_imaginary * b_real);
+            return c;
+        }
+
+        public Complex Substract(double a_real, double a_imaginary, double b_real, double b_imaginary)
+        {
+            Complex c = new Complex();
+            c.Real = a_real - b_real;
+            c.Imaginary = a_imaginary - b_imaginary;
+            return c;
+        }
+    }
+}
