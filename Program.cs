5c9a82f0d6ff53ca22415b847851a8f18811eb43
Łukasz Andrusiewicz <andrusiewiczl19263@student.pswbp.pl>
2020-11-10 01:07:27 +0100

    lab3 fourth commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Calculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hostt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(Service1)))
+            {
+                //host.AddServiceEndpoint(typeof(IServicee1), new BasicHttpBinding(), "http://localhost:52850/Service1.svc");
+                host.Open(); 
+                Console.WriteLine("Calculator.Service1 hosted."); 
+                Console.ReadKey(true);
+                host.Close();
+            }
+
+        }
+    }
+}
